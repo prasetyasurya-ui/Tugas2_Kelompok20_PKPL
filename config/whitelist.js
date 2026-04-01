dfd27fd6aaa1e0d9444a25c8644b22650a325a29
JaaTzy14 <mirzaradithya1410@gmail.com>
2026-04-01 10:03:35 +0700

    feat(mirza): add more email to whitelist

--- config/whitelist.js
+++ config/whitelist.js
@@ -2,7 +2,7 @@ const WHITELIST_EMAILS = [
     'mirzaradithyar@gmail.com',
     'rahmanmafaza0@gmail.com',
     'prasetya.suryai@ui.ac.id',
-    ''
+    'williamjonnatan31@gmail.com'
 ];
 
 module.exports = WHITELIST_EMAILS;
